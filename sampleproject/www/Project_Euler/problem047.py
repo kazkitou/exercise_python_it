99eef9db4ab20f2f6b49b8d287c4a2601ce260af
kazkitou <kazkitou9080@gmail.com>
2021-02-04 13:50:33 +0900

    add comment

--- sampleproject/www/Project_Euler/problem047.py
+++ sampleproject/www/Project_Euler/problem047.py
@@ -3,19 +3,25 @@ import itertools
 
 def problem_47() -> int:
     '''Distinct primes factors'''
+    serial_num = 4
     multiple_prime_factor_list = list()
     for i in itertools.count(647):
         multiple_prime_factor_list.append(
             get_multiple_prime_factor_decomposition(get_prime_factor_decomposition(i)))
-        if len(multiple_prime_factor_list[-1]) != 4:
+        if len(multiple_prime_factor_list[-1]) != serial_num:
             multiple_prime_factor_list.clear()
-        if len(multiple_prime_factor_list) == 4:
-            return i - 3
+        if len(multiple_prime_factor_list) == serial_num:
+            return i - (serial_num - 1)
     return -1
 
 def get_multiple_prime_factor_decomposition(prime_factor: dict) -> set:
     '''
         引数prime_factor（素因数分解を辞書形式でまとめた結果）から累乗を除いて掛け算の形で返す
+            それぞれ3つの異なる素因数を持つ連続する3つの数が最初に現れるのは:
+                644 = 2^2 × 7 × 23
+                645 = 3   × 5 × 43
+                646 = 2   × 17 × 19
+            上記説明があるため、単純に素因数分解結果の底の種類を数えるだけではダメなので、累乗の計算結果を返す
     '''
     multiple_prime_factor = set()
     for base_num in prime_factor.keys():
