8916d0e7998877f379ce8273b53f3c4f8b85d328
kazkitou <kazkitou9080@gmail.com>
2021-02-09 13:58:53 +0900

    use formatter black

--- sampleproject/book/BeginningPython3_O_REILLY/chapter4/4-2.py
+++ sampleproject/book/BeginningPython3_O_REILLY/chapter4/4-2.py
@@ -1,12 +1,12 @@
 if __name__ == "__main__":
     guess_me = int(input("input int guess_me = "))
     start = int(input("input int start = "))
-    while True :
-        if start < guess_me :
+    while True:
+        if start < guess_me:
             print("too low")
-        elif start == guess_me :
+        elif start == guess_me:
             print("found it!")
-        else :
+        else:
             print("oops")
             break
         start += 1
