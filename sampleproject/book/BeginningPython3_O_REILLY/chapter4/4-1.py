62629a470aa06fefcbc7648b635b13b52dfa167e
kazkitou <kazkitou9080@gmail.com>
2021-02-09 13:38:57 +0900

    use formatter

--- sampleproject/book/BeginningPython3_O_REILLY/chapter4/4-1.py
+++ sampleproject/book/BeginningPython3_O_REILLY/chapter4/4-1.py
@@ -1,8 +1,9 @@
-guess_me = int(input("input int guess_me = "))
-if guess_me < 7:
-    print("too low")
-elif guess_me > 7:
-    print("too high")
-else:
-    # guess_me == 7
-    print("just right")
+if __name__ == "__main__":
+    guess_me = int(input("input int guess_me = "))
+    if guess_me < 7:
+        print("too low")
+    elif guess_me > 7:
+        print("too high")
+    else:
+        # guess_me == 7
+        print("just right")
--- sampleproject/book/BeginningPython3_O_REILLY/chapter4/4-2.py
+++ sampleproject/book/BeginningPython3_O_REILLY/chapter4/4-2.py
@@ -1,11 +1,12 @@
-guess_me = int(input("input int guess_me = "))
-start = int(input("input int start = "))
-while True :
-    if start < guess_me :
-        print("too low")
-    elif start == guess_me :
-        print("found it!")
-    else :
-        print("oops")
-        break
-    start += 1
+if __name__ == "__main__":
+    guess_me = int(input("input int guess_me = "))
+    start = int(input("input int start = "))
+    while True :
+        if start < guess_me :
+            print("too low")
+        elif start == guess_me :
+            print("found it!")
+        else :
+            print("oops")
+            break
+        start += 1
