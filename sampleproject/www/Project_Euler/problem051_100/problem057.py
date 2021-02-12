134cd893718e2e3a5353dcabf1b9327f4177ed4d
kazkitou <kazkitou9080@gmail.com>
2021-02-12 17:00:55 +0900

    Project Euler Problem 57 fixed

--- sampleproject/www/Project_Euler/problem051_100/problem057.py
+++ sampleproject/www/Project_Euler/problem051_100/problem057.py
@@ -1,42 +1,45 @@
 """Project Euler Problem 57"""
-import itertools
+import sys
 import math
 
 
 def problem_57() -> int:
     """	Square root convergents"""
     counter = 0
-    for i in itertools.count(1, 5):
+    # 再帰処理による呼び出し回数の最大値を変更する
+    recursionlimit = sys.getrecursionlimit()
+    sys.setrecursionlimit(1010)
+
+    for i in range(1, 1001):
         fraction_list = get_fraction(1, get_continued_fraction_expansion(i))
-        print(fraction_list)
         if judge_numerator_digit_is_bigger_than_denominator(fraction_list):
             counter += 1
+
+    # 再帰処理による呼び出し回数の最大値を元に戻す
+    sys.setrecursionlimit(recursionlimit)
     return counter
 
 
 def get_fraction(num: int, fraction_list: list) -> list:
     """a + b/c -> return fraction[a*c+b, c]"""
-    print('get_fraction', num, fraction_list)
-    tmp_list = [num*fraction_list[1]+fraction_list[0], fraction_list[1]]
+    tmp_list = [num * fraction_list[1] + fraction_list[0], fraction_list[1]]
     max_gcd = math.gcd(tmp_list[0], tmp_list[1])
-    return list([tmp_list[0]//max_gcd, tmp_list[1]//max_gcd])
+    return list([tmp_list[0] // max_gcd, tmp_list[1] // max_gcd])
 
 
 def get_continued_fraction_expansion(ex_cnt: int) -> list:
     """
     平方根の連分数展開の公式を用いて、√2をリスト形式で[分子, 分母]の値を返す
-        ex_cnt == 1 -> 1 +                1/2
-        ex_cnt == 2 -> 1 + 1/          (2+1/2)
-        ex_cnt == 3 -> 1 + 1/     (2+1/(2+1/2))
-        ex_cnt == 4 -> 1 + 1/(2+1/(2+1/(2+1/2)))
+        ex_cnt == 1  ->  3 /  2 -> 1 +  1 /  2 -> 1 + 1/           2
+        ex_cnt == 2  ->  7 /  5 -> 1 +  2 /  5 -> 1 + 1/          (2+1/2)
+        ex_cnt == 3  -> 17 / 12 -> 1 +  5 / 12 -> 1 + 1/     (2+1/(2+1/2))
+        ex_cnt == 4  -> 41 / 29 -> 1 + 12 / 29 -> 1 + 1/(2+1/(2+1/(2+1/2)))
             計算を{ex_cnt}回繰り返して求めた値を返す
     """
-    tmp_list = list()
     if ex_cnt <= 1:
-        return list([1, 2])
-    else:
-        tmp_list = get_continued_fraction_expansion(ex_cnt-1)
-    return get_fraction(2, list([tmp_list[1], tmp_list[0]]))
+        return [1, 2]
+    tmp_reverse = get_fraction(2, get_continued_fraction_expansion(ex_cnt - 1))
+    return [tmp_reverse[1], tmp_reverse[0]]
 
 
 def judge_numerator_digit_is_bigger_than_denominator(fraction_list: list) -> bool:
@@ -49,5 +52,4 @@ def judge_numerator_digit_is_bigger_than_denominator(fraction_list: list) -> boo
 
 
 if __name__ == "__main__":
-#    print(problem_57())
-    print('last', get_fraction(1, get_continued_fraction_expansion(2)))
+    print(problem_57())
